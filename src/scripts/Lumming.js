cd39d0b7257b6418f00694a35f9781e9101d7ae6
Germain Shigo <germain.shigo@ensimag.grenoble-inp.fr>
2015-05-22 11:57:12 +0200

    prototypes lummings

--- src/scripts/Lumming.js
+++ src/scripts/Lumming.js
@@ -19,27 +19,34 @@ var ColorEnum = Object.freeze({
 	}
 });
 
-function Lumming() {
+function Lumming(name) {
 };
 
 function VisibleLumming(color) {
 	this.color = color;
 };
+VisibleLumming.prototype = new Lumming;
 
 function LowFreqLumming() {
 };
+LowFreqLumming.prototype = new Lumming;
 
 function RadioLumming() {
 };
+RadioLumming.prototype = new LowFreqLumming;
 
 function MicroLumming() {
 };
+MicroLumming.prototype = new LowFreqLumming;
 
 function HighFreqLumming() {
 };
+HighFreqLumming.prototype = new Lumming;
 
 function XRayLumming() {
 };
+XRayLumming.prototype = new HighFreqLumming;
 
 function GammaLumming() {
 };
+GammaLumming.prototype = new HighFreqLumming;
