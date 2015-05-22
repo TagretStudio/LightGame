2c7cdaa067490d7e5961900039c20bb204e71225
Akseniya Armyanski <akseniyaarmyanski@gmail.com>
2015-05-22 14:15:55 +0200

    Level2 vide aussi

---
+++ src/scripts/Level2.js
@@ -0,0 +1,18 @@
+BasicGame.Level2 = function (game){
+	
+};
+
+BasicGame.Level2.prototype = {
+
+	preload: function () {
+		
+	},
+
+	create: function () {
+		
+	},
+	
+	update: function () {
+		
+	}
+};
