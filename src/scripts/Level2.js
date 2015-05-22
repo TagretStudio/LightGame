f7d73444c01d6aa0aa74722b6f981f2c1f6be4b9
Penel Frédéric <frederic.penel@ensimag.grenoble-inp.fr>
2015-05-22 15:50:29 +0200

    debut leverl 2

--- src/scripts/Level2.js
+++ src/scripts/Level2.js
@@ -1,18 +1,19 @@
 BasicGame.Level2 = function (game){
-	
+	this.platform = null;
+	this.lum = null;
 };
 
 BasicGame.Level2.prototype = {
 
 	preload: function () {
-		
+
 	},
 
 	create: function () {
-		
+
 	},
-	
+
 	update: function () {
-		
+
 	}
 };
