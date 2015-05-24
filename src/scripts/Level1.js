8e89715d16c9574192188b3e5e120f91a2137ccc
Akseniya Armyanski <akseniyaarmyanski@gmail.com>
2015-05-24 12:08:29 +0200

    merge

--- src/scripts/Level1.js
+++ src/scripts/Level1.js
@@ -1,7 +1,7 @@
 BasicGame.Level1 = function (game){
 	this.platforms = null;
 	this.lums = null;
-	this.lum = null;
+	this.doors = null;
 };
 
 BasicGame.Level1.prototype = {
@@ -9,13 +9,15 @@ BasicGame.Level1.prototype = {
 	preload: function () {
 		this.load.image('sky', 'src/media/img/sky.png');
 		this.load.image('platform', 'src/media/img/platform.png');
+		this.load.spritesheet('door', 'src/media/img/lumming_cyan.png', 32, 32);
 		this.load.spritesheet('lumming', 'src/media/img/lumming_magenta.png', 32, 32);
-	    if (music != null && music.isPlaying == true) {
-		music.fadeOut(700);
-	    }
-	    music = this.add.audio('game_over_music');
-	    music.loop = true;
-	    music.play();
+
+		if (music != null && music.isPlaying == true) {
+			music.fadeOut(700);
+		}
+		music = this.add.audio('game_over_music');
+		music.loop = true;
+		music.play();
 	},
 
 	create: function () {
@@ -26,29 +28,64 @@ BasicGame.Level1.prototype = {
 
 		platforms = this.add.group();
 		platforms.enableBody = true;
-		var platform;
-		platform = platforms.create(0, this.world.height - 64, 'platform');
-		platform.body.immovable = true;
+		{
+			platforms.create(  0-200, 100, 'platform');
+			platforms.create(300-200, 200, 'platform');
+			platforms.create(600-200, 300, 'platform');
+			platforms.create(900-200, 280, 'platform');
+		}
+		platforms.forEach(function(p){p.body.immovable=true});
 
-		//lums = this.add.group();
-		//var lum;
-		//lum = lums.create(0, 0, 'lumming');
-		//this.physics.arcade.enable(lum);
-		//lum.animations.add('left', [0, 1, 2, 3], 10, true);
-		//lum.animations.add('right', [5, 6, 7, 8], 10, true);
-		lum = this.add.sprite(0, 0, 'lumming');
-		this.physics.arcade.enable(lum);
-		lum.animations.add('left', [4, 5, 6, 7], 10, true);
-		lum.animations.add('right', [8, 9, 10, 11], 10, true);
+		doors = this.add.group();
+		doors.enableBody = true;
+		var door;
+		{
+			door = doors.create(600-200, 270, 'door');
+		}
+		doors.forEach(function(door) {
+			door.animations.add('anim', [8, 9, 10, 11], 10, true);
+			door.animations.play('anim');
+		});
+
+		lums = this.add.group();
+		var lum;
+		{
+			lum = lums.create(0, 0, 'lumming');
+			this.physics.arcade.enable(lum); //WARNING noter que cette ligne là doit se trouver avant les accès à "body.velocity"
+			lum.body.velocity.x = 100;
+		}
+		lums.forEach(function(lum) {
+			lum.animations.add('left', [4, 5, 6, 7], 10, true);
+			lum.animations.add('right',  [8, 9, 10, 11], 10, true);
+			lum.body.gravity.y = 300;
+			lum.body.bounce.x = 1;
+		});
+
 	},
 	
 	update: function () {
-		//lums.forEach(
-		//	function(lum) {
-		//		lum.animations.play('right');
-		//	}
-		//);
-		lum.animations.play('right');
+		this.physics.arcade.collide(lums, doors, mayExit, null, this);
+		this.physics.arcade.collide(lums, platforms);
+
+		lums.forEach(
+			function(lum) {
+				if (lum.body.velocity.x > 0) {
+					lum.animations.play('right');
+				} else if (lum.body.velocity.x < 0) {
+					lum.animations.play('left');
+				} else {
+					lum.animations.stop();
+				}
+			}
+		);
 
 	}
 };
+
+// TODO quand on aura des classes de lummings, la collision porte/lummming dépendra de la couleur de chacun
+// ça serait bien aussi une petite animation quand on gagne, plutôt que de passer direct au niveau suivant
+function mayExit(lum, door) {
+	lum.kill();
+	//compter les lummings restants ici et s'il n'y en a plus, passer au niveau suivant
+	this.state.start('Level2');
+}
--- src/scripts/Level2.js
+++ src/scripts/Level2.js
@@ -1,74 +1,120 @@
 BasicGame.Level2 = function (game){
-this.lum1 = null;
-this.lums = null;
-this.platforms = null;
-this.arrive = null;
+	this.platforms = null;
+	this.white_lums = null;
+	this.blue_lums = null;
+	this.blue_doors = null;
+	this.blue_filters = null;
 };
 
 BasicGame.Level2.prototype = {
 
-
 	preload: function () {
-		this.stage.backgroundColor = '85b5e5';
+		this.load.image('sky', 'src/media/img/sky.png');
 		this.load.image('platform', 'src/media/img/platform.png');
-		this.load.spritesheet('lumming', 'src/media/img/lumming_magenta.png', 32, 32);
-	  this.load.image('arrive', 'src/media/img/lummings-logo.png');
-		  if (music != null && music.isPlaying == true) {
-		music.fadeOut(700);
-	    }
-	    music = this.add.audio('game_over_music');
-	    music.loop = true;
-	    music.play();
+		this.load.spritesheet('blue_door', 'src/media/img/lumming_cyan.png', 32, 32);
+		this.load.spritesheet('white_lumming', 'src/media/img/lumming_white.png', 32, 32);
+		this.load.spritesheet('blue_lumming', 'src/media/img/lumming_blue.png', 32, 32);
+		this.load.image('blue_filter', 'src/media/img/diamond.png');
 
+		if (music != null && music.isPlaying == true) {
+			music.fadeOut(700);
+		}
+		music = this.add.audio('game_over_music');
+		music.loop = true;
+		music.play();
 	},
 
 	create: function () {
 		this.physics.startSystem(Phaser.Physics.ARCADE);
-		platforms=this.add.group();
+
+		var sky = this.add.sprite(0, 0, 'sky');
+		sky.scale.set(1024/800, 768/600);
+
+		platforms = this.add.group();
 		platforms.enableBody = true;
-		platforms.create(0, this.world.height - 200, 'platform');
-		platforms.create(200, this.world.height - 200, 'platform');
-		platforms.setAll('body.immovable', true);
-		this.physics.arcade.enable(platforms);
+		{
+			platforms.create(  0-200, 100, 'platform');
+			platforms.create(300-200, 200, 'platform');
+			platforms.create(600-200, 300, 'platform');
+			platforms.create(900-200, 280, 'platform');
+			platforms.create(200-200, 280, 'platform');
+		}
+		platforms.forEach(function(p){p.body.immovable=true});
 
-		lums = this.add.group();
-	//	this.physics.arcade.enable(lums);
-		lum1 = lums.create(0, this.world.height -300, 'lumming');
-		this.physics.arcade.enable(lum1);
-		lum1.body.collideWorldBounds = true;
-		lum1.body.gravity.y = 450;
+		blue_doors = this.add.group();
+		blue_doors.enableBody = true;
+		var door;
+		{
+			door = blue_doors.create(700-200, 270, 'blue_door');
+		}
+		blue_doors.forEach(function(door) {
+			door.animations.add('anim', [8, 9, 10, 11], 10, true);
+			door.animations.play('anim');
+		});
 
-		arrive = this.add.sprite(300, this.world.height - 300 , 'arrive');
-		this.physics.arcade.enable(arrive);
-		arrive.body.immovable = true;
-	//	this.physics.arcade.enable(lum1);
-		lum1.animations.add('left', [4, 5, 6, 7], 10, true);
-		lum1.animations.add('right', [8, 9, 10, 11], 10, true);
+		var lum;
 
+		white_lums = this.add.group();
+		{
+			lum = white_lums.create(0, 0, 'white_lumming');
+			this.physics.arcade.enable(lum); //WARNING noter que cette ligne là doit se trouver avant les accès à "body.velocity"
+			lum.body.velocity.x = 100;
+		}
+		white_lums.forEach(function(lum) {
+			lum.animations.add('left', [4, 5, 6, 7], 10, true);
+			lum.animations.add('right',  [8, 9, 10, 11], 10, true);
+			lum.body.gravity.y = 300;
+			lum.body.bounce.x = 1;
+		});
 
-
+		blue_lums = this.add.group();
+		//vide pour l'instant mais un lumming blanc peut devenir bleu
+		
+		blue_filters = this.add.group();
+		blue_filters.enableBody = true;
+		var filter;
+		{
+			filter = blue_filters.create(600-200, 270, 'blue_filter');
+		}
+		
 	},
-
-
-
+	
 	update: function () {
-		this.physics.arcade.collide(lum1, platforms);
-		lum1.animations.play('right');
-		lum1.body.velocity.x = 50;
-	//	this.physics.arcade.collide(lum1, arrive);
-		this.physics.arcade.collide(lum1, arrive, functionarrive, null, this);
+		this.physics.arcade.collide(blue_lums, blue_doors, mayExit, null, this);
+		this.physics.arcade.overlap(white_lums, blue_filters, toBlue, null, this);
+		this.physics.arcade.collide(white_lums, platforms);
+		this.physics.arcade.collide(blue_lums, platforms);
 
+		white_lums.forEach(lumAnim);
+		blue_lums.forEach(lumAnim);
 	}
 };
 
-
-	function functionarrive(){
-		lum1.animations.play('left');
-		this.time.events.add(1000, ftransition, this);
-
+function lumAnim(lum) {
+	if (lum.body.velocity.x > 0) {
+		lum.animations.play('right');
+	} else if (lum.body.velocity.x < 0) {
+		lum.animations.play('left');
+	} else {
+		lum.animations.stop();
 	}
+}
 
-	function ftransition(){
-		this.state.start('Preloader');
+function toBlue(lum, filter) {
+	var newlum = blue_lums.create(lum.body.position.x, lum.body.position.y, 'blue_lumming');
+	this.physics.arcade.enable(newlum); //WARNING noter que cette ligne là doit se trouver avant les accès à "body.velocity"
+	newlum.body.velocity.x = lum.body.velocity.x;
+	newlum.body.velocity.y = lum.body.velocity.y;
+	newlum.animations.add('left', [4, 5, 6, 7], 10, true);
+	newlum.animations.add('right',  [8, 9, 10, 11], 10, true);
+	newlum.body.bounce.x = 1;
+	lum.kill();
+}
 
-	}
+// TODO quand on aura des classes de lummings, la collision porte/lummming dépendra de la couleur de chacun
+// ça serait bien aussi une petite animation quand on gagne, plutôt que de passer direct au niveau suivant
+function mayExit(lum, door) {
+	lum.kill();
+	//compter les lummings restants ici et s'il n'y en a plus, passer au niveau suivant
+	this.state.start('Boot');
+}
