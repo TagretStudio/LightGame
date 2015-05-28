af14f2790f8a4808d2801debc7f7cdb69013ac6e
Germain Shigo <germain.shigo@ensimag.grenoble-inp.fr>
2015-05-28 16:23:53 +0200

    ajout des autres sprites visibles aux lummings visibles

--- src/scripts/VisibleLummingFactory.js
+++ src/scripts/VisibleLummingFactory.js
@@ -16,8 +16,14 @@ define(['LummingFactory'], function(LummingFactory) {
         init: function(game) {
             _game = game;
             LummingFactory.init(_game);
+            _game.load.spritesheet('lumming_black', 'src/media/img/lumming_black.png', 32, 32, 16);
             _game.load.spritesheet('lumming_blue', 'src/media/img/lumming_blue.png', 32, 32, 16);
+            _game.load.spritesheet('lumming_cyan', 'src/media/img/lumming_cyan.png', 32, 32, 16);
+            _game.load.spritesheet('lumming_green', 'src/media/img/lumming_green.png', 32, 32, 16);
+            _game.load.spritesheet('lumming_magenta', 'src/media/img/lumming_magenta.png', 32, 32, 16);
             _game.load.spritesheet('lumming_red', 'src/media/img/lumming_red.png', 32, 32, 16);
+            _game.load.spritesheet('lumming_white', 'src/media/img/lumming_white.png', 32, 32, 16);
+            _game.load.spritesheet('lumming_yellow', 'src/media/img/lumming_yellow.png', 32, 32, 16);
 
 
         },
