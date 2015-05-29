5af6e63a12935cb2bfdbc0c672862e38adaf0bfd
Germain Shigo <germain.shigo@ensimag.grenoble-inp.fr>
2015-05-29 15:23:50 +0200

    tg Ewen

--- src/scripts/PlatformFactory.js
+++ src/scripts/PlatformFactory.js
@@ -3,7 +3,12 @@
     var _game = null;
     
     var Platform = function(game, x, y, isPb) {
-        
+        this.isPb = isPb;
+        if (isPb) {
+            //this.spriteName = ''; A CHANGER une fois qu'on aura un sprite pour le plomb
+        } else {
+            this.spriteName = 'platform';
+        }
         
     }
     
@@ -11,6 +16,10 @@
         init : function(game) {
             _game = game;
             _game.load.image('platform', 'src/media/img/platform.png');
+        },
+        
+        create : function(x, y, isPb) {
+            return (new Lumming(_game, x, y, isPb));
         }
     }
  })
