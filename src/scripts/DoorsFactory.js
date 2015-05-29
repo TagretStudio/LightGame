1cf012941f46d46a7727579e6c83e62c02a4bbf8
Penel Frédéric <frederic.penel@ensimag.grenoble-inp.fr>
2015-05-29 17:59:04 +0200

    des portes git add  src/scripts/DoorsFactory.js  src/scripts/Level1.js

--- src/scripts/DoorsFactory.js
+++ src/scripts/DoorsFactory.js
@@ -1,6 +1,25 @@
 define(['Items'], function(Items) {
   var _game = null;
 
+  var Doors = function(color, x, y){
+    this.color = color;
+
+    this.spriteName = 'door_' + color;
+    Items.Item.call(this, this.spriteName, x, y);
+    this.body.setSize(32, 32);
+    this.animations.add('animDoor', [], 10, true);
+    this.frame = 0;
+
+
+  }
+
+  Doors.prototype = Object.create(Items.Item.prototype);
+  Doors.prototype.constructor = Doors;
+
+  Doors.prototype.update = function(){
+    this.animations.play('animDoor');
+  }
+
   return {
     init : function(game){
       _game = game;
@@ -12,7 +31,9 @@ define(['Items'], function(Items) {
       _game.load.spritesheet('door_red', 'src/media/img/door_red.png', 32, 32, 16);
       _game.load.spritesheet('door_white', 'src/media/img/door_white.png', 32, 32, 16);
       _game.load.spritesheet('door_yellow', 'src/media/img/door_yellow.png', 32, 32, 16);
-
+    },
+    create : function(color, x, y){
+      return (new Doors(color, x, y));
     }
   }
 })
--- src/scripts/Level1.js
+++ src/scripts/Level1.js
@@ -1,10 +1,10 @@
-define(['Images', 'LummingFactory', 'VisibleLummingFactory', 'ColorEnum', 'MusicFactory', 'PlatformFactory'],
-	   function(Images, LummingFactory, VisibleLummingFactory, ColorEnum, MusicFactory, PlatformFactory) {
+define(['Images', 'LummingFactory', 'VisibleLummingFactory', 'ColorEnum', 'MusicFactory', 'PlatformFactory', 'DoorsFactory'],
+	   function(Images, LummingFactory, VisibleLummingFactory, ColorEnum, MusicFactory, PlatformFactory, DoorsFactory) {
 	var _game = null;
 	var _etapesuivante = null;
 	var platforms = null;
 	var _groupLum = null;
-
+	var _groupdoors = null;
 	var zizik = null;
 
 	var _level1 = {
@@ -18,6 +18,7 @@ define(['Images', 'LummingFactory', 'VisibleLummingFactory', 'ColorEnum', 'Music
 		//	Lumming.init(_game);
 			VisibleLummingFactory.init(_game);
 			PlatformFactory.init(_game);
+			DoorsFactory.init(_game);
 		},
 		create : function(){
 			zizik.play();
@@ -37,8 +38,10 @@ define(['Images', 'LummingFactory', 'VisibleLummingFactory', 'ColorEnum', 'Music
 			platforms.add(platform2);
 			platforms.add(platform3);
 			platforms.add(platform4);
-			
-			
+
+
+
+
 			doors = this.add.group();
 			doors.enableBody = true;
 			var door;
@@ -54,10 +57,15 @@ define(['Images', 'LummingFactory', 'VisibleLummingFactory', 'ColorEnum', 'Music
 
 			lum1 = VisibleLummingFactory.create('blue', 0, 0, 100);
 			lum2 = VisibleLummingFactory.create('red', 100, 0, -200);
-
-
+			lum3 = VisibleLummingFactory.create('yellow', 500, 0, -100);
+			_groupdoors = this.add.group();
+			door1 = DoorsFactory.create('red', 400, 100);
+			door2 = DoorsFactory.create('yellow', 0, 400);
+			_groupdoors.add(door1);
+			_groupdoors.add(door2);
 			_groupLum.add(lum1);
 			_groupLum.add(lum2);
+			_groupLum.add(lum3);
 
 			_game.startText = _game.add.text(0, 500, 'cliquez pour commencer', { fontSize: '32px', fill: '#000' });
 			_game.input.onDown.add(function () {if(_game.paused) {_game.paused = false;_game.startText.text = '';}},_game);
@@ -73,6 +81,12 @@ define(['Images', 'LummingFactory', 'VisibleLummingFactory', 'ColorEnum', 'Music
 						p.update();
 					}
 					)
+				_groupdoors.forEach(
+					function(p){
+						p.update();
+					}
+				)
+
 		}
 
 
