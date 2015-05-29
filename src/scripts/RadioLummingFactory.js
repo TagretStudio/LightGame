928133b9643ebf6ea36985c74a943bd6446e22a6
Thomas Niebojewski <thomas.niebojewski@ensimag.grenoble-inp.fr>
2015-05-29 14:48:56 +0200

    Factory Radio commencée

---
+++ src/scripts/RadioLummingFactory.js
@@ -0,0 +1,25 @@
+define(['LummingFactory'], function(LummingFactory) {
+
+    var _game = null;
+
+    var RadioLumming = function(game, x, y, vitesseX, visibleLevel /*TODO*/) {
+	this.sprite = 'src/media/img/lumming_radio';
+	LummingFactory.Lumming.call(this, game, 'lumming_radio', x, y, vitesseX);
+    }
+
+    RadioLumming.prototype = Object.create(LummingFactory.Lumming.prototype);
+
+    RadioLumming.prototype.constructor = RadioLumming;
+
+    return {
+        init: function(game) {
+            _game = game;
+            LummingFactory.init(_game);
+	    _game.load.spritesheet('lumming_radio', 'src/media/img/lumming_radio.png', 32, 32, 16);
+	},
+
+	create: function(x, y, vitesseX, visibleLevel /*TODO*/) {
+            return (new RadioLumming(_game, x, y, vitesseX, visibleLevel));
+        }
+    }
+})
