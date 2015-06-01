3944399591459e97f569e87a9383516a28ee59a2
Thomas Niebojewski <thomas.niebojewski@ensimag.grenoble-inp.fr>
2015-06-01 23:00:51 +0200

    MiroirFactory attaqué

---
+++ src/scripts/MiroirFactory.js
@@ -0,0 +1,62 @@
+define(['Items', 'VisionEnum'], function(Items, VisionEnum) {
+	
+	var _game = null;
+	
+	var Filter = function(color, x, y) {
+		this.color = color;
+		if (ColorEnum.getName(color) == ColorEnum.getColorEnum().RED
+			|| ColorEnum.getName(color) == ColorEnum.getColorEnum().GREEN
+			|| ColorEnum.getName(color) == ColorEnum.getColorEnum().BLUE) {
+			this.isAdditive = true;
+		} else {
+			this.isAdditive = false;
+		}
+		
+		this.spriteName = 'filter_' + ColorEnum.getName(color);
+		Items.Item.call(this, this.spriteName, x, y);
+		this.body.setSize(32, 32);
+		this.animations.add('animFilter', [], 10, true);
+		this.frame = 0;
+	}
+	
+	Filter.prototype = Object.create(Items.Item.prototype);
+	Filter.prototype.constructor = Filter;
+	
+	Filter.prototype.update = function() {
+		this.animations.play('animFilter');
+	}
+	
+	Filter.prototype.isAdditive = function() {
+		return this.isAdditive;
+	}
+	
+	Filter.prototype.getColor = function() {
+		return this.color;
+	}
+	
+	return {
+		init: function(game) {
+			_game = game;
+			Items.init(_game);
+			_game.load.spritesheet('filter_blue', 'src/media/img/filter_blue.png', 32, 32, 11);
+			_game.load.spritesheet('filter_cyan', 'src/media/img/filter_cyan.png', 32, 32, 11);
+			_game.load.spritesheet('filter_green', 'src/media/img/filter_green.png', 32, 32, 11);
+			_game.load.spritesheet('filter_magenta', 'src/media/img/filter_magenta.png', 32, 32, 11);
+			_game.load.spritesheet('filter_red', 'src/media/img/filter_red.png', 32, 32, 11);
+			_game.load.spritesheet('filter_yellow', 'src/media/img/filter_yellow.png', 32, 32, 11);
+		},
+		
+		create: function(color, x, y) {
+			return (new Filter(color, x, y));
+		},
+		
+		isAdditive: function() {
+			return isAdditive();
+		},
+		
+		getColorValue: function() {
+			return ColorEnum.getValue(getColor);
+		}
+	}
+	
+})
