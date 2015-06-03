f7d3f374f0556c8a732905c3f1ce6e9a226476ed
Penel Frédéric <frederic.penel@ensimag.grenoble-inp.fr>
2015-06-03 10:19:48 +0200

    oké

--- src/scripts/MiroirFactory.js
+++ src/scripts/MiroirFactory.js
@@ -1,7 +1,7 @@
 define(['Items', 'VisionEnum'], function(Items, VisionEnum) {
-	
+
 	var _game = null;
-	
+
 	var Miroir = function(x, y, isVertical) {
 		if (isVertical) {
 			this.isVertical = true;
@@ -10,7 +10,7 @@ define(['Items', 'VisionEnum'], function(Items, VisionEnum) {
 			this.isVertical = false;
 		    this.spriteName = 'Horizontal';
 		}
-		
+
 		this.spriteName = 'miroir_' + this.spriteName;
 		Items.Item.call(this, this.spriteName, x, y);
 	    this.body.bounce = 1.1;
@@ -20,29 +20,29 @@ define(['Items', 'VisionEnum'], function(Items, VisionEnum) {
 		this.body.setSize(32, 16);
 	    }
 	}
-	
+
 	Miroir.prototype = Object.create(Items.Item.prototype);
 	Miroir.prototype.constructor = Miroir;
-	
+
 	Miroir.prototype.isVertical = function() {
 		return this.isVertical;
 	}
-	
+
 	return {
 		init: function(game) {
 			_game = game;
 			Items.init(_game);
-			_game.load.image('miroir_Horizontal', 'src/media/img/mirroirOrizontal.png');
-			_game.load.spritesheet('miroir_Vertical', 'src/media/img/mirroirVertical.png');
+			_game.load.image('miroir_Horizontal', 'media/img/mirroirOrizontal.png');
+			_game.load.spritesheet('miroir_Vertical', 'media/img/mirroirVertical.png');
 			},
-		
+
 		create: function(x, y, isVertical) {
 			return (new Miroir(x, y, isVertical));
 		},
-		
+
 		isVertical: function() {
 			return isVertical();
 		},
 	}
-	
+
 })
