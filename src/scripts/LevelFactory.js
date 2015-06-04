e60b5680c50338c80ca8f2823e19896947f7236c
Germain Shigo <germain.shigo@ensimag.grenoble-inp.fr>
2015-06-04 16:02:40 +0200

    rien ne s'affiche... pourquoi?

--- src/scripts/LevelFactory.js
+++ src/scripts/LevelFactory.js
@@ -41,11 +41,41 @@ define(['Images', 'LummingFactory', 'VisibleLummingFactory', 'ColorEnum',
 			_game.physics.startSystem(Phaser.Physics.ARCADE);
 			_currentVision = VisionEnum.getVisionEnum().VISIBLE;
 			
-			this.levelStruct = LevelStructure.create(indexLevel);	
+			this.levelStruct = LevelStructure.create(indexLevel);
+			
+			_nbLummingsV = 2;
+			text = _game.add.text(750, 0, _nbLummingsSaved+'/'+_nbLummingsV, {align: "center"});
+			button_menu = _game.add.button(32,0, 'buttonDiamond', actionOnMenu, _game);
+			button_restart = _game.add.button(650,0,'buttonRefresh', actionOnRestart, _game);
+
+			_menu = MenuFactoryTest.create();
+			ItemsLevel.reinit(_game);
+			var cliquez = this.add.sprite(100, 300, 'cliquez');
+			cliquez.scale.set(0.7, 0.7);
+			_game.input.onDown.add(function () {if(_game.paused) {_game.paused = false;cliquez.destroy();;}},_game);
+			_game.paused = true;
 		},
 		
 		update: function() {
+			_menu.update();
+			_game.physics.arcade.collide(_groupLum, _groupPlatforms);
+			_game.physics.arcade.overlap(_groupLum, _groupDoors, mayExit, null, _game);
+
+			_groupLum.forEach(
+				function(p){
+					p.update(_currentVision);
+				}
+			)
 			
+			_groupDoors.forEach(
+				function(p){
+					p.update();
+				}
+			)
+			
+			if (_nbLummingsV == _nbLummingsSaved) {
+				Transition.nextState('MainMenu', _music);
+			}
 		}
 		
 	}
@@ -53,6 +83,43 @@ define(['Images', 'LummingFactory', 'VisibleLummingFactory', 'ColorEnum',
 	function changeLevel(indexLevel) {
 		delete this.levelStruct;
 		this.levelStruct = LevelStructure.create(indexLevel);
+	}
+	
+	function mayExit(lum, door){
+		if (lum.getDefautVision() == 2) {
+			var exit = lum.collideWithDoor(door);
+			if (exit == 1){
+				_nbLummingsSaved = _nbLummingsSaved +1;
+				text.setText( _nbLummingsSaved + '/'+ _nbLummingsV);
+			}
+		}
+	}
+	
+	function actionOnRestart() {
+		var background = _game.add.sprite(0, 0, 'transitionBackground');
+		var logo = _game.add.sprite(184, 265, 'logo');
+		if (_music != null) {
+			_music.getMusic().fadeOut(700);
+			_music.getMusic().onFadeComplete.dispatch();
+			_music.getMusic().onFadeComplete.addOnce(function() {
+				_music = null;
+				_currentVision = VisionEnum.getVisionEnum().VISIBLE;
+				_game.state.start('LevelFactory');
+			}, _game);
+		}
+	}
+	
+	function actionOnMenu() {
+	   var background = _game.add.sprite(0, 0, 'transitionBackground');
+	   var logo = _game.add.sprite(184, 265, 'logo');
+	   if (_music != null) {
+			_music.getMusic().fadeOut(700);
+			_music.getMusic().onFadeComplete.dispatch();
+			_music.getMusic().onFadeComplete.addOnce(function() {
+				_music = null;
+				_game.state.start('MainMenu');
+			}, _game);
+		}
 	}
 	
 	return {
