fc076b8f0fee77590352ca6781ec71926b6949d5
Penel Frédéric <frederic.penel@ensimag.grenoble-inp.fr>
2015-06-08 11:53:53 +0200

    ajout bouton level 1

--- src/scripts/LevelSelection.js
+++ src/scripts/LevelSelection.js
@@ -28,10 +28,14 @@ define(['./Images', './MusicFactory' ,'./MainMenu', 'Transition', 'LevelFactory'
      Transition.nextState('LevelFactory', _music);
    }
 
+//288, 64
+
    	var _levelSelection = {
   		preload : function(){
         _pointLogo = new Phaser.Point(_game.world.centerX - 216, _game.world.centerY - 66);
         _pointButtons = new Phaser.Point(_game.world.centerX - 20, _game.world.centerY - 100);
+        _game.load.spritesheet('level1Sp', 'media/img/niveau1Spritesheet.png', 288, 64);
+
   		},
 
   		create : function(){
@@ -39,9 +43,9 @@ define(['./Images', './MusicFactory' ,'./MainMenu', 'Transition', 'LevelFactory'
 
         _buttons = _game.add.group();
         _buttons.scale.set(_game.world.width/1024, _game.world.height/768);
-        _buttons.add(_buttonCredits = _game.make.button(Math.round(Math.random()*10)*-5, _game.world.centerY + 160, 'button', actionCredits, _game, 3, 4, 5));
-        _buttons.add(_buttonPlay = _game.make.button(Math.round(Math.random()*10)*-5, _game.world.centerY + 100, 'button', actionPlay, _game, 0, 1, 2));
-        _buttons.add(_buttonQuit = _game.make.button(Math.round(Math.random()*10)*-5, _game.world.centerY + 220, 'button', actionQuit, _game, 6, 7, 8));
+        _buttons.add(_buttonCredits = _game.make.button(Math.round(Math.random()*10)*-5, _game.world.centerY + 160, 'level1Sp', actionCredits, _game, 0, 1, 2));
+        _buttons.add(_buttonPlay = _game.make.button(Math.round(Math.random()*10)*-5, _game.world.centerY + 100, 'level1Sp', actionPlay, _game, 0, 1, 2));
+        _buttons.add(_buttonQuit = _game.make.button(Math.round(Math.random()*10)*-5, _game.world.centerY + 220, 'level1Sp', actionQuit, _game, 0, 1, 2));
 
   		},
 
