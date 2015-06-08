90089116bd0df5941208186e682db11b763d7eda
Penel Frédéric <frederic.penel@ensimag.grenoble-inp.fr>
2015-06-08 14:40:38 +0200

    menu fonsionnel

--- src/scripts/LevelSelection.js
+++ src/scripts/LevelSelection.js
@@ -1,60 +1,57 @@
 define(['./Images', './MusicFactory' ,'./MainMenu', 'Transition', 'LevelFactory'], function(Images, MusicFactory, MainMenu, Transition, LevelFactory){
 	var _game = null;
 	var _etapesuivante = null;
+  var _currentLevel = 1;
 	var _space = null;
 	var _music = null;
   var _buttons = null;
-  var _buttonPlay = null;
-  var _buttonCredits = null;
-  var _buttonQuit = null;
-  var _pointButtons = null;
+  var _button = null;
   var _pointLogo =null;
 
-   function actionPlay() {
-     _buttonPlay.kill();
-     LevelFactory.setLevel(1);
-     Transition.nextState('LevelFactory', _music);
-   }
 
-   function actionCredits(){
-     _buttonCredits.kill();
-     LevelFactory.setLevel(2);
-     Transition.nextState('LevelFactory', _music);
-   }
+  function actionPlay(){
+    LevelFactory.setLevel(_currentLevel);
+    Transition.nextState('LevelFactory', _music);
+  }
 
-   function actionQuit(){
-     _buttonQuit.kill();
-     LevelFactory.setLevel(3);
-     Transition.nextState('LevelFactory', _music);
-   }
 
-//288, 64
+  function actionButton(niveau){
+  //  LevelFactory.setLevel(niveau);
+    _currentLevel += niveau;
+    //Transition.nextState('LevelFactory', _music);
+  }
 
    	var _levelSelection = {
   		preload : function(){
         _pointLogo = new Phaser.Point(_game.world.centerX - 216, _game.world.centerY - 66);
-        _pointButtons = new Phaser.Point(20, 20);
-        _game.load.spritesheet('levelSp', 'media/img/niveauSpritesheet.png', 288, 64);
+        _pointButtons = new Phaser.Point(_game.world.centerX , _game.world.centerY - 20);
+        _game.load.spritesheet('buttonplusmoins', 'media/img/buttonplusmoins.png', 38, 38);
+        _game.load.spritesheet('button', 'media/img/MenuButtons.png', 274, 71);
+
 
   		},
 
   		create : function(){
         Images.boot().create();
-
         _buttons = _game.add.group();
-        _buttons.scale.set(_game.world.width/1024, _game.world.height/768);
-        _buttons.add(_buttonPlay = _game.make.button(Math.round(Math.random()*10)*-5, 0, 'levelSp', actionPlay, _game, 0, 1, 2));
-        _buttons.add(_buttonCredits = _game.make.button(Math.round(Math.random()*10)*-5, 60, 'levelSp', actionCredits, _game, 3, 4, 5));
+  			_buttons.scale.set(_game.world.width/1024, _game.world.height/768);
+  			_buttons.add(_buttonPlus = _game.make.button(_game.world.centerX*1024/800, _game.world.centerY, 'buttonplusmoins', function(){actionButton(1);}, _game, 0, 1, 2));
+        _buttons.add(_buttonMoins = _game.make.button(_game.world.centerX*1024/800,_game.world.centerY + 150, 'buttonplusmoins', function(){actionButton(-1);}, _game, 3, 4, 5));
+        _buttons.add(_buttonPlay = _game.make.button(_game.world.centerX*1024/800, _game.world.centerY + 300, 'button', actionPlay, _game, 0, 1, 2));
 
-        _buttons.add(_buttonQuit = _game.make.button(Math.round(Math.random()*10)*-5,  120, 'levelSp', actionQuit, _game, 0, 1, 2));
+        _buttonPlus.anchor.set(0.5, 0.5);
+        _buttonPlus.scale.set(2,2);
+        _buttonMoins.anchor.set(0.5, 0.5);
+        _buttonMoins.scale.set(2,2);
+
+        text = _game.add.text(_game.world.centerX, _game.world.centerY, _currentLevel, {align: "center"});
+				text.anchor.set(0.5,0.5);
+        text.scale.set(2,2)
 
   		},
 
   		update :function(){
-        _buttons.forEach(
-  					function(butt){
-  						butt.x += Math.min(30, (_pointButtons.x- butt.x)/4);
-  					});
+        text.setText(_currentLevel);
   		}
   	}
 
