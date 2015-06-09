68afe7b8f0810fd63c4397dd259ea941095da025
akseniya <akseniyaarmyanski@gmail.com>
2015-06-09 09:32:49 +0200

    test plateforme plomb

--- src/scripts/LevelStructure.js
+++ src/scripts/LevelStructure.js
@@ -395,6 +395,7 @@ define(['PlatformFactory', 'LummingFactory', 'VisibleLummingFactory', 'ColorEnum
 		var p;
 
 		p = _game.add.sprite(ice.left, ice.y, 'platforms',3);
+		p.isPb = false;
 		p.anchor.set(1,0);
 		ls.groupPlatforms.add(p);
 		p.body.checkCollision.down = false;
@@ -404,6 +405,7 @@ define(['PlatformFactory', 'LummingFactory', 'VisibleLummingFactory', 'ColorEnum
 		p.collisionsSet = true;
 
 		p = _game.add.sprite(ice.right, ice.y, 'platforms',5);
+		p.isPb = false;
 		p.anchor.set(0,0);
 		ls.groupPlatforms.add(p);
 		p.body.checkCollision.down = false;
@@ -413,6 +415,7 @@ define(['PlatformFactory', 'LummingFactory', 'VisibleLummingFactory', 'ColorEnum
 		p.collisionsSet = true;
 
 		p = _game.add.sprite(ice.left, ice.y + ice.height, 'platforms',12);
+		p.isPb = false;
 		p.anchor.set(1,0);
 		ls.groupPlatforms.add(p);
 		p.body.checkCollision.down = false;
@@ -432,6 +435,7 @@ define(['PlatformFactory', 'LummingFactory', 'VisibleLummingFactory', 'ColorEnum
 		ls.groupPlatforms.add(p);
 
 		p = _game.add.sprite(x, ice.y + ice.height, 'platforms',14);
+		p.isPb = false;
 		p.anchor.set(0,0);
 		ls.groupPlatforms.add(p);
 		p.width = w;
@@ -444,6 +448,20 @@ define(['PlatformFactory', 'LummingFactory', 'VisibleLummingFactory', 'ColorEnum
 		ice.kill();
 	}
 
+	platformPlomb = function(groupPlatforms, x, y, w) {
+		/*
+		var dummy = _game.add.sprite(0,0,'plomb');
+		var sw = dummy.width;
+		dummy.kill();
+		*/
+		var p;
+		p = _game.add.sprite(x,y,'plomb');
+		p.isPb = true;
+		groupPlatforms.add(p);
+		p.isPb = true;
+		p.width = w;
+	}
+
 	platform = function(groupPlatforms, x, y, w, l, r) {
 		if (l==null) l=false;
 		if (r==null) r=false;
@@ -453,13 +471,16 @@ define(['PlatformFactory', 'LummingFactory', 'VisibleLummingFactory', 'ColorEnum
 		var p;
 		for (var i=0; i< Math.floor(Math.floor(w/sw)/2)+1; i++) {
 			p = _game.add.sprite(x + w/2 - i*sw, y,'platforms',6);
+			p.isPb = false;
 			groupPlatforms.add(p);
 			p.anchor.set(0,0);
 			p = _game.add.sprite(x + w/2 + i*sw, y,'platforms',6);
+			p.isPb = false;
 			groupPlatforms.add(p);
 			p.anchor.set(1,0);
 		}
 		p = _game.add.sprite(x,y,'platforms',4);
+		p.isPb = false;
 		if (!l) p.frame+=2;
 		groupPlatforms.add(p);
 		p.body.checkCollision.down = false;
@@ -468,6 +489,7 @@ define(['PlatformFactory', 'LummingFactory', 'VisibleLummingFactory', 'ColorEnum
 		p.body.checkCollision.up = false;
 		p.collisionsSet = true;
 		p = _game.add.sprite(x+w,y,'platforms',2);
+		p.isPb = false;
 		if (!r) p.frame+=4;
 		groupPlatforms.add(p);
 		p.anchor.set(1,0);
@@ -510,6 +532,7 @@ define(['PlatformFactory', 'LummingFactory', 'VisibleLummingFactory', 'ColorEnum
 		init: function(game) {
 			_game = game;
 			_game.load.spritesheet('platforms', 'media/img/tiles3.png',32,32);
+            _game.load.image('plomb', 'media/img/plombVertical.png');
 		},
 
 		create: function(indexLevel) {
